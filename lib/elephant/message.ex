85605e54aa369ec5512ddf1e04d365b9e57e8ea7
eteubert <eric@ericteubert.de>
2018-02-13 11:49:49 +0800

    parse STOMP messages

--- lib/elephant/message.ex
+++ lib/elephant/message.ex
@@ -3,8 +3,11 @@ defmodule Elephant.Message do
   defstruct command: nil, headers: [], body: nil
 
   alias Elephant.Message
+  require Logger
 
-  @eol <<13, 10>>
+  @cr <<13>>
+  @lf <<10>>
+  @eol @cr <> @lf
   @ascii_null <<0>>
 
   def format(%Message{command: :send, headers: headers, body: body}),
@@ -20,16 +23,55 @@ defmodule Elephant.Message do
     do: format("CONNECT", headers)
 
   defp format(command, headers) when is_binary(command) do
-    command <> @eol <> headers(headers) <> @eol <> @eol <> @ascii_null
+    command <> @eol <> format_headers(headers) <> @eol <> @eol <> @ascii_null
   end
 
   defp format(command, headers, body) when is_binary(command) do
-    command <> @eol <> headers(headers) <> @eol <> @eol <> body <> @ascii_null
+    command <> @eol <> format_headers(headers) <> @eol <> @eol <> body <> @ascii_null
   end
 
-  def headers(headers) do
+  def format_headers(headers) do
     headers
     |> Enum.map(fn {k, v} -> "#{k}:#{v}" end)
     |> Enum.join(@eol)
+  end
+
+  def parse(<<"CONNECT", @eol, tail::binary>>) do
+    parse_headers(tail, [], %Message{command: :connect})
+  end
+
+  def parse(<<"CONNECT", @lf, tail::binary>>) do
+    parse_headers(tail, [], %Message{command: :connect})
+  end
+
+  defp parse_headers(tail, headers, message) do
+    [line, tail] = Regex.split(~r/\r?\n/, tail, parts: 2)
+
+    cond do
+      String.contains?(line, ":") ->
+        parse_headers(tail, [line | headers], message)
+
+      String.length(line) == 0 ->
+        parse_body(tail, %{message | headers: normalize_headers(headers)})
+
+      true ->
+        raise "Parse error. Expected header or newline, got: #{line}"
+    end
+  end
+
+  def normalize_headers(headers) do
+    headers
+    |> Enum.reverse()
+    |> Enum.map(fn header ->
+      [k, v] = Regex.split(~r{:}, header, parts: 2)
+      {k, v}
+    end)
+  end
+
+  defp parse_body(tail, message) do
+    body_size = byte_size(tail) - 1
+    <<body::binary-size(body_size), 0>> = tail
+
+    %{message | body: body}
   end
 end
--- test/elephant_message_test.exs
+++ test/elephant_message_test.exs
@@ -27,4 +27,44 @@ defmodule ElephantMessageTest do
 
     assert str == "CONNECT\r\nkey:value\r\n\r\n" <> <<0>>
   end
+
+  test "parses messages" do
+    message = "CONNECT\r\nkey:value\r\n\r\ntest" <> <<0>>
+
+    assert Message.parse(message) == %Message{
+             command: :connect,
+             headers: [{"key", "value"}],
+             body: "test"
+           }
+  end
+
+  test "parses messages with multiple headers" do
+    message = "CONNECT\r\nkey:value\r\nkey2:value2\r\n\r\ntest" <> <<0>>
+
+    assert Message.parse(message) == %Message{
+             command: :connect,
+             headers: [{"key", "value"}, {"key2", "value2"}],
+             body: "test"
+           }
+  end
+
+  test "parses messages with LF but no CR" do
+    message = "CONNECT\nkey:value\n\ntest" <> <<0>>
+
+    assert Message.parse(message) == %Message{
+             command: :connect,
+             headers: [{"key", "value"}],
+             body: "test"
+           }
+  end
+
+  test "parses messages without headers" do
+    message = "CONNECT\r\n\r\n" <> <<0>>
+
+    assert Message.parse(message) == %Message{
+             command: :connect,
+             headers: [],
+             body: ""
+           }
+  end
 end

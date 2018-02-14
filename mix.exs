4f355c7b6d50551710134e36249aece623d872f1
eteubert <eric@ericteubert.de>
2018-02-14 20:08:38 +0800

    improve mix.exs

--- mix.exs
+++ mix.exs
@@ -4,13 +4,24 @@ defmodule Elephant.MixProject do
   def project do
     [
       app: :elephant,
+      description: "A STOMP client to listen to, for example, AMQ.",
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
+      source_url: "https://github.com/eteubert/elephant",
+      docs: [
+        main: "Elephant"
+      ]
+    ]
+  end
 
-      # Docs
-      name: "Elephant"
+  defp package() do
+    [
+      maintainers: ["Eric Teubert"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/eteubert/elephant"}
     ]
   end
 

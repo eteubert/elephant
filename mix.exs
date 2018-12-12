0af70573f49992acad3bcaf8728c42e0310f7471
Eric Teubert <eric@ericteubert.de>
2018-12-12 12:21:04 +0100

    release 0.2.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elephant.MixProject do
     [
       app: :elephant,
       description: "A STOMP client to listen to, for example, AMQ.",
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
